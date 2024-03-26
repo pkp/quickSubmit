9348dd1041cf7ef14c11fd67356cf53888036b6a
Jonas Raoni Soares da Silva <github@raoni.org>
2024-03-26 13:34:45 +0300

    pkp/pkp-lib#2644 Fixed the abstract requirement

--- QuickSubmitForm.php
+++ QuickSubmitForm.php
@@ -68,6 +68,11 @@ class QuickSubmitForm extends Form
                 throw new \Exception('Submission not in context!');
             }
 
+            $sectionId = $request->getUserVar('sectionId');
+            if (!empty($sectionId)) {
+                $this->_submission->setSectionId($sectionId);
+            }
+
             $this->_submission->setLocale($this->getDefaultFormLocale());
             $publication = $this->_submission->getCurrentPublication();
             $publication->setData('locale', $this->getDefaultFormLocale());
